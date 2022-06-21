caff43adfb912937cab81cf440d4ab0d22c38383
CypherCript <45295109+CypherCript@users.noreply.github.com>
2022-06-20 21:05:23 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Loader
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 1500);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
